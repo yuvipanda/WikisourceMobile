bf31b6a0564ef241f7c96f28cd6a4518fa0d20ae
YuviPanda <yuvipanda@gmail.com>
2012-05-17 18:35:46 +0530

    URLEncode the title when it is part of the API Url
    Passing unencoded URLs to the URLCache plugin causes it to barf
    with absolutely no intelligible error messages

--- assets/www/js/page.js
+++ assets/www/js/page.js
@@ -133,7 +133,7 @@
 	// Returns an API URL that makes a request that retreives this page
 	// Should mimic params from Page.requestFromTitle
 	Page.prototype.getAPIUrl = function() {
-		return app.baseUrlForLanguage(this.lang) + '/w/api.php?format=json&action=mobileview&page=' + this.title + '&redirects=1&prop=sections&sections=all&sectionprop=level|line&noheadings=true';
+		return app.baseUrlForLanguage(this.lang) + '/w/api.php?format=json&action=mobileview&page=' + encodeURIComponent(this.title) + '&redirects=1&prop=sections&sections=all&sectionprop=level|line&noheadings=true';
 	};
 
 	Page.prototype.getCanonicalUrl = function() {
