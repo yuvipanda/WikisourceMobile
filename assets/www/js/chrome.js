yuvipanda/WikisourceMobile

commit 407c639463c11b4d8c28ed6d4f2016e3124b9731

Prevent click event from being fired multiple times
Fix for https://bugzilla.wikimedia.org/show_bug.cgi?id=37454

assets/www/js/chrome.js
@@ -142,7 +142,6 @@ window.chrome = function() {
 			MobileFrontend.references.init($("#content")[0], true, {onClickReference: onClickReference} );
 
 			app.setFontSize(preferencesDB.get('fontSize'));
-			chrome.initContentLinkHandlers("#main");
 			savedPages.doMigration().done(function() {
 				chrome.loadFirstPage().done(function() {
 					$("#migrating-saved-pages-overlay").hide();
@@ -267,13 +266,12 @@ window.chrome = function() {
 	}
 
 	function initContentLinkHandlers(selector) {
-		$(selector).delegate('a', 'click', function(event) {
+		$(selector).find('a').bind('click', function(event) {
 			var target = this,
 				url = target.href,             // expanded from relative links for us
 				href = $(target).attr('href'); // unexpanded, may be relative
 
 			event.preventDefault();
-
 			if (url.match(new RegExp("^https?://([^/]+)\." + PROJECTNAME + "\.org/wiki/"))) {
 				// ...and load it through our intermediate cache layer.
 				app.navigateToPage(url);

commit e1418a779e5eaf07266f9b14e4c128b152634496

Fix bug introduced with 407c639463c11b4d8c28ed6d4f2016e3124b9731

assets/www/js/chrome.js
@@ -38,11 +38,13 @@ window.chrome = function() {
 	}
 
 	function populateSection(sectionID) {
-		var $contentBlock = $("#content_" + sectionID);
+		var selector = "#content_" + sectionID;
+		var $contentBlock = $(selector);
 		if(!$contentBlock.data('populated')) {
 			var sectionHtml = app.curPage.getSectionHtml(sectionID);
 			$contentBlock.append($(sectionHtml)).data('populated', true);
 			MobileFrontend.references.init($contentBlock[0], false, {animation: 'none'});
+			chrome.initContentLinkHandlers(selector);
 		} 
 	}
 