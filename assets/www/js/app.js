3a0b1665725e78df2f0ceeef6d18d15ac345198c
YuviPanda <yuvipanda@gmail.com>
2012-06-13 23:24:36 +0530

    Parse the text from the API by default into JSON

--- assets/www/js/app.js
+++ assets/www/js/app.js
@@ -246,7 +246,10 @@ window.app = function() {
 			// Making this 'text' and parsing the JSON ourselves makes things much easier
 			// Than making it as 'JSON' for pre-processing via dataFilter
 			// See https://forum.jquery.com/topic/datafilter-function-and-json-string-result-problems
-			dataType: 'text'
+			dataType: 'text',
+			dataFilter: function(text) {
+				return JSON.parse(text);
+			}
 		};
 		var options = $.extend(defaultOptions, extraOptions);
 		return $.ajax(options);
