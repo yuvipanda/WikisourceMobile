c89834b1ae23675863b65f75188b107c554eec37
YuviPanda <yuvipanda@gmail.com>
2012-06-23 00:34:21 +0530

    Make pages scroll properly on iOS 5.x when viewed from nearby

--- assets/www/js/app.js
+++ assets/www/js/app.js
@@ -195,6 +195,9 @@ window.app = function() {
 			console.log("Navigating to " + title);
 			if(options.hideCurrent) {
 				$("#content").show();
+				// see http://forrst.com/posts/iOS_scrolling_issue_solved-rgX
+				// Fix for bug causing page to not scroll in iOS 5.x when visited from nearby
+				chrome.scrollTo("#content", 0);
 			}			
 		});
 		return d;
